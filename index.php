c4217cb78c8d43554206fc44f95db9dd077eb942
jawuermkr <guitarro92@hotmail.com>
2022-08-17 10:52:26 -0500

    primera entrega

---
+++ index.php
@@ -0,0 +1,67 @@
+<html lang="es">
+    <head>
+        <link href="css/estilos.css" rel="stylesheet">
+        <title> Surti Nippon SAS </title>
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Surti Nippon SAS</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href="#">Nosotros</a>
+                <a class="nav-link" href="#">Galería</a>
+                <a class="nav-link" href="#">Contacto</a>
+            </div>
+            </div>
+        </div>
+        </nav>
+        <div class="text-center m-5">
+            <h2>Surti Nippon SAS<h2>
+            <h3>¡Distribuidores especializados en calidad y servicio!</h3>
+            <a href="https://api.whatsapp.com/send?phone=573154937449" target="_blank"><button class="btn btn-outline-success m-5">¡Habla con un asesor!</button></a>
+        </div>
+    </header>
+    <div class="white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="img/motor.png" width="90%">
+            </div>
+            <div class="col-md-6">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.632900089301!2d-74.0737753857575!3d4.659375743313122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a53d3ea584d%3A0x320f5dc0313b8275!2sCra.%2027%20%2367-12%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1660751143648!5m2!1ses-419!2sco" width="100%" height="360" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <footer class="footer m-5 text-white">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Surti Nippon SAS</h3><br/>
+                    <p>&copy <?php echo date('Y'); ?></p>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                    Carrera 27 #67-12<br/>
+                    601 5724212<br/>
+                    3154937449<br/>
+                    surti.nippon@gmail.com<br/>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</doby>
+</html>
