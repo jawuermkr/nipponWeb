970be8013413598e3fe9a93c6e7410f2602cf6f7
jawuermkr <guitarro92@hotmail.com>
2022-08-18 22:45:28 -0500

    Se agregan marcas, formulario y nuevos detalles

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <!--- Link para WhatsApp Flotante --->    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 <body>
+    <!--- WhatsApp Flotante --->
+    <a href="https://api.whatsapp.com/send?phone=573154937449" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
+    <!--- WhatsApp Flotante --->
     <header>
         <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
@@ -37,14 +42,84 @@
                 <img src="img/motor.png" width="90%">
             </div>
             <div class="col-md-6">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
+            <div class="col-md-2">
+                <img src="img/chevrolet.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/citroen.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/honda.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/hyundai.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/kia.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/mitsubishi.png" width="90%"/>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-2">
+                <img src="img/nissan.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/peugeot.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/renault.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/subaru.png" width="90%"/>
+            </div>
+            <div class="col-md-2">
+                <img src="img/toyota.png" width="90%"/>
+            </div>
+            <div class="col-md-1"></div>
         </div>
     </div>
     </div>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.632900089301!2d-74.0737753857575!3d4.659375743313122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a53d3ea584d%3A0x320f5dc0313b8275!2sCra.%2027%20%2367-12%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1660751143648!5m2!1ses-419!2sco" width="100%" height="360" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+    <div class="container">
+        <div class="row text-white mt-5 mb-5">
+            <div class="col-md-6">
+                <h3>¡Contacta!</h3>
+                <form>
+                    <label>Nombre</label>
+                    <input class="form-control" type="text" name="nombre" />
+                    <label>Celular</label>
+                    <input class="form-control" type="text" name="celular" />
+                    <label>asunto</label>
+                    <input class="form-control" type="text" name="asunto" />
+                    <label>Mensaje</label>
+                    <textarea class="form-control" type="text" name="mensaje"></textarea>
+                    <input class="btn btn-outline-success form-control" type="submit" name="btnSend" value="Enviar" />
+                </form>
+            </div>
+            <div class="col-md-6">
+                <h3>Medios de pago</h3>
+                <p>Ofrecemos diferentes medios que garantizan la comodidad a la hora de hacer una transacción.</p>
+                <center><img src="img/pago.png" width="60%"/><center>
+            </div>
+        </div>
+    </div>
+    
+    <div class="white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="img/motor.png" width="90%">
+            </div>
+        </div>
+    </div>
+    </div>
     <footer class="footer m-5 text-white">
         <div class="container text-center">
             <div class="row">
