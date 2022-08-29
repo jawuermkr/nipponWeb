d3cad6c95e0f22067421df68924cb734e9c96349
jawuermkr <guitarro92@hotmail.com>
2022-08-28 21:46:09 -0500

    cambios agregados

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <link href="css/estilos.css" rel="stylesheet">
         <title> Surti Nippon SAS </title>
         <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
+        <link href="fonts/fonts.css" rel="stylesheet"> 
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
         <!-- JavaScript Bundle with Popper -->
@@ -14,6 +15,19 @@
     <!--- WhatsApp Flotante --->
     <a href="https://api.whatsapp.com/send?phone=573154937449" class="whatsapp" target="_blank"> <i class="fa fa-whatsapp whatsapp-icon"></i></a>
     <!--- WhatsApp Flotante --->
+    
+    <!--- Redes Sociales --->
+    <div class="social">
+		<ul>
+			<li><a href="#" target="_blank" class="icon-facebook"></a></li>
+			<li><a href="tel:6015724212" target="_blank" class="icon-phone"></a></li>
+			<li><a href="https://api.whatsapp.com/send?phone=573154937449" target="_blank" class="icon-mobile"></a></li>
+			<li><a href="mailto:surti.nippon@gmail.com" class="icon-mail"></a></li>
+		</ul>
+	</div>
+    <!--- Redes Sociales --->
+
+
     <header>
         <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
@@ -40,20 +54,39 @@
     <div class="container">
         <div class="row">
             <h4>¡Productos destacados!</h4>
-            <div class="col-md-3">
-                <img src="img/citroen.png" width="90%"/>
+
+            <div class="col-md-2">
+            <img src="img/product01.png" width="90%"/>
+            <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
             </div>
-            <div class="col-md-3">
-                <img src="img/honda.png" width="90%"/>
+            <div class="col-md-2">
+                <img src="img/product02.png" width="90%"/>
+                <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
             </div>
-            <div class="col-md-3">
-                <img src="img/peugeot.png" width="90%"/>
+            <div class="col-md-2">
+                <img src="img/product03.png" width="90%"/>
+                <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
             </div>
-            <div class="col-md-3">
-                <img src="img/subaru.png" width="90%"/>
+            <div class="col-md-2">
+                <img src="img/product04.png" width="90%"/>
+                <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
+            </div>
+            <div class="col-md-2">
+                <img src="img/product05.png" width="90%"/>
+                <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
+            </div>
+            <div class="col-md-2">
+                <img src="img/product06.png" width="90%"/>
+                <a class="form-control btn btn-info" href="pages/contacto.php"><buuton>¡Cotizar!</button></a>
             </div>
 
-
+        </div>
+    </div>
+    </div>
+    
+    <div class="white">
+    <div class="container">
+        <div class="row">
             <div class="col-md-6">
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
@@ -81,21 +114,27 @@
                 <h4>VALORES</h4> 
                 <p>Somos una empresa transparente con sentido de responsabilidad y puntualidad con nosotros y nuestros clientes, en la cual nos enfocamos en la excelencia de nuestros servicios para poder orientarlos en calidad y referencias para sus vehículos.</p>
             </div>
-            <div class="col-md-3">
-                <img src="img/citroen.png" width="90%"/>
-            </div>
-            <div class="col-md-3">
-                <img src="img/honda.png" width="90%"/>
-            </div>
-            <div class="col-md-3">
-                <img src="img/peugeot.png" width="90%"/>
-            </div>
-            <div class="col-md-3">
-                <img src="img/subaru.png" width="90%"/>
-            </div>
         </div>
     </div>
     </div>
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <img src="img/citroen.png" width="90%"/>
+        </div>
+        <div class="col-md-3">
+            <img src="img/honda.png" width="90%"/>
+        </div>
+        <div class="col-md-3">
+            <img src="img/peugeot.png" width="90%"/>
+        </div>
+        <div class="col-md-3">
+            <img src="img/subaru.png" width="90%"/>
+        </div>
+    </div>
+    </div>
+
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.632900089301!2d-74.0737753857575!3d4.659375743313122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a53d3ea584d%3A0x320f5dc0313b8275!2sCra.%2027%20%2367-12%2C%20Bogot%C3%A1!5e0!3m2!1ses-419!2sco!4v1660751143648!5m2!1ses-419!2sco" width="100%" height="360" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     
     <div class="container">
@@ -126,10 +165,36 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <h4>NOSOTROS</h4> 
+            <p>Somos una empresa dedica a la importación y venta de autopartes de la más alta calidad en HONDA, SUBARU, PEUGEOT y CITROEN con más de 10 años de experiencia, en la cual destacamos nuestro compromiso en la atención a nuestros clientes brindándoles un gran portafolio de repuestos de gama alta para el arreglo, rendimiento y seguridad de sus vehículos. <br/>
+            Contamos con el personal calificado con conocimiento de sus vehículos en el cual podrá realizar sus consultas y dudas para el cambio y arreglo de piezas, nos ubicamos en la ciudad de Bogotá hacemos despachos a todo el país.</p>
             </div>
             <div class="col-md-6">
-                <img src="img/motor.png" width="90%">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img/auto4.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/auto5.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/auto6.png" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                <img src="img/auto7.png" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+            </div>
             </div>
         </div>
     </div>
@@ -137,11 +202,20 @@
     <footer class="footer m-5 text-white">
         <div class="container text-center">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-4">
+                    <p>¡Horarios de atención!</p>
+                    <p>Lunes a viernes<br/>
+                    8:00 am - 6:00 pm</p>
+                    <p>Sábados<br/>
+                    8:30 am - 2:00 pm</p>
+                </div>
+                <div class="col-md-4">
                     <h3>Surti Nippon SAS</h3><br/>
                     <p>&copy <?php echo date('Y'); ?></p>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
+                    <p>¡Contacto!</p>
+                    
                     <p>
                     Carrera 27 #67-12<br/>
                     601 5724212<br/>
